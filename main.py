a272795bd2ac0c1fc02fab7d88c19c845cb0572d
Alan Gardner <urfolomeus@gmail.com>
2022-07-30 13:17:16 +0100

    Adding background images

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 
+# Set up game
 SCREEN_OPTS = {
   "width": 1000,
   "height": 1000,
@@ -11,12 +12,21 @@ SCREEN_OPTS = {
 screen = pygame.display.set_mode((SCREEN_OPTS["width"], SCREEN_OPTS["height"]))
 pygame.display.set_caption("Platformer")
 
+# Load assets
+img_bg = pygame.image.load("./assets/img/sky.png")
+img_sun = pygame.image.load("./assets/img/sun.png")
+
+# Game loop
 run = True
 
 while run :
+  screen.blit(img_bg, (0, 0))
+  screen.blit(img_sun, (100, 100))
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
+
+  pygame.display.update()
 
 pygame.quit()
