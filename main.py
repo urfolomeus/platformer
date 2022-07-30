de0597b34f858fbb9822e118e7d70c7e529e2025
Alan Gardner <urfolomeus@gmail.com>
2022-07-30 13:09:42 +0100

    Some code style tweaks

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from pygame.locals import *
 pygame.init()
 
 SCREEN_OPTS = {
-    "width": 1000,
-    "height": 1000,
+  "width": 1000,
+  "height": 1000,
 }
 
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((SCREEN_OPTS["width"], SCREEN_OPTS["height"]))
 pygame.display.set_caption("Platformer")
 
 run = True
+
 while run :
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
 
 pygame.quit()
