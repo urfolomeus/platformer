9accb6c5fc058c2fa61b1b60aa202ef4393242ab
Alan Gardner <urfolomeus@gmail.com>
2022-07-30 16:24:39 +0100

    Adjusting opts

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from pygame.locals import *
 pygame.init()
 
 # Set up game
-SCREEN_OPTS = {
+OPTS = {
   "width": 1000,
   "height": 1000,
+  "title": "Platformer",
+  "tile_size": 50,
 }
-TILE_SIZE = 50
 
-screen = pygame.display.set_mode((SCREEN_OPTS["width"], SCREEN_OPTS["height"]))
-pygame.display.set_caption("Platformer")
+screen = pygame.display.set_mode((OPTS["width"], OPTS["height"]))
+pygame.display.set_caption(OPTS["title"])
 
 # Load assets
 img_bg = pygame.image.load("./assets/img/sky.png")
@@ -20,8 +21,8 @@ img_sun = pygame.image.load("./assets/img/sun.png")
 # Draw grid (helps us see where things will go on screen)
 def draw_grid():
 	for line in range(0, 20):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * TILE_SIZE), (SCREEN_OPTS["width"], line * TILE_SIZE))
-		pygame.draw.line(screen, (255, 255, 255), (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_OPTS["height"]))
+		pygame.draw.line(screen, (255, 255, 255), (0, line * OPTS["tile_size"]), (OPTS["width"], line * OPTS["tile_size"]))
+		pygame.draw.line(screen, (255, 255, 255), (line * OPTS["tile_size"], 0), (line * OPTS["tile_size"], OPTS["height"]))
 
 class World:
   def __init__(self, data):
@@ -43,10 +44,10 @@ class World:
       screen.blit(tile[0], tile[1])
 
   def __build_tile__(self, img_name, x_start, y_start):
-    img = pygame.transform.scale(img_name, (TILE_SIZE, TILE_SIZE))
+    img = pygame.transform.scale(img_name, (OPTS["tile_size"], OPTS["tile_size"]))
     img_rect = img.get_rect()
-    img_rect.x = x_start * TILE_SIZE
-    img_rect.y = y_start * TILE_SIZE
+    img_rect.x = x_start * OPTS["tile_size"]
+    img_rect.y = y_start * OPTS["tile_size"]
     tile = (img, img_rect)
     self.tile_list.append(tile)
 
