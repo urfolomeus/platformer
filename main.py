6de3942c2e73e1ab90e018e21b48f91a41e05b76
Alan Gardner <urfolomeus@gmail.com>
2022-07-30 13:51:12 +0100

    Adding a draw_grid function

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ SCREEN_OPTS = {
   "width": 1000,
   "height": 1000,
 }
+TILE_SIZE = 200
 
 screen = pygame.display.set_mode((SCREEN_OPTS["width"], SCREEN_OPTS["height"]))
 pygame.display.set_caption("Platformer")
@@ -16,12 +17,20 @@ pygame.display.set_caption("Platformer")
 img_bg = pygame.image.load("./assets/img/sky.png")
 img_sun = pygame.image.load("./assets/img/sun.png")
 
+# Draw grid (helps us see where things will go on screen)
+def draw_grid():
+	for line in range(0, 20):
+		pygame.draw.line(screen, (255, 255, 255), (0, line * TILE_SIZE), (SCREEN_OPTS["width"], line * TILE_SIZE))
+		pygame.draw.line(screen, (255, 255, 255), (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_OPTS["height"]))
+
 # Game loop
 run = True
 
 while run :
   screen.blit(img_bg, (0, 0))
   screen.blit(img_sun, (100, 100))
+
+  draw_grid()
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
