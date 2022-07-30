3027486b4a40ade0a291aef7188243e03fdf2661
Alan Gardner <urfolomeus@gmail.com>
2022-07-30 16:41:07 +0100

    Refactor to world class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 
 from config import config
+from world import world
 
 pygame.init()
 
@@ -19,58 +20,6 @@ def draw_grid():
 		pygame.draw.line(screen, (255, 255, 255), (0, line * config.tile_size), (config.width, line * config.tile_size))
 		pygame.draw.line(screen, (255, 255, 255), (line * config.tile_size, 0), (line * config.tile_size, config.height))
 
-class World:
-  def __init__(self, data):
-    self.tile_list = []
-
-    # load images
-    img_dirt = pygame.image.load("./assets/img/dirt.png")
-    img_grass = pygame.image.load("./assets/img/grass.png")
-
-    for row_idx, row in enumerate(data):
-      for tile_idx, tile in enumerate(row):
-        if tile == 1:
-          self.__build_tile__(img_dirt, tile_idx, row_idx)
-        if tile == 2:
-          self.__build_tile__(img_grass, tile_idx, row_idx)
-
-  def draw(self):
-    for tile in self.tile_list:
-      screen.blit(tile[0], tile[1])
-
-  def __build_tile__(self, img_name, x_start, y_start):
-    img = pygame.transform.scale(img_name, (config.tile_size, config.tile_size))
-    img_rect = img.get_rect()
-    img_rect.x = x_start * config.tile_size
-    img_rect.y = y_start * config.tile_size
-    tile = (img, img_rect)
-    self.tile_list.append(tile)
-
-# Build world
-world_data = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
-  [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
-  [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
-  [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-  [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
-  [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
-  [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-  [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-  [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
-world = World(world_data)
-
 # Game loop
 run = True
 
@@ -78,7 +27,7 @@ while run :
   screen.blit(img_bg, (0, 0))
   screen.blit(img_sun, (100, 100))
 
-  world.draw()
+  world.draw(screen)
   # draw_grid()
 
   for event in pygame.event.get():
---
+++ world.py
@@ -0,0 +1,56 @@
+import pygame
+
+from config import config
+
+class World:
+  def __init__(self, data):
+    self.tile_list = []
+
+    # load images
+    img_dirt = pygame.image.load("./assets/img/dirt.png")
+    img_grass = pygame.image.load("./assets/img/grass.png")
+
+    for row_idx, row in enumerate(data):
+      for tile_idx, tile in enumerate(row):
+        if tile == 1:
+          self.__build_tile__(img_dirt, tile_idx, row_idx)
+        if tile == 2:
+          self.__build_tile__(img_grass, tile_idx, row_idx)
+
+  def draw(self, screen):
+    for tile in self.tile_list:
+      screen.blit(tile[0], tile[1])
+
+  def __build_tile__(self, img_name, x_start, y_start):
+    img = pygame.transform.scale(img_name, (config.tile_size, config.tile_size))
+    img_rect = img.get_rect()
+    img_rect.x = x_start * config.tile_size
+    img_rect.y = y_start * config.tile_size
+    tile = (img, img_rect)
+    self.tile_list.append(tile)
+
+# Build world
+world_data = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+  [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+  [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+  [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+  [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+world = World(world_data)
